0b5fc21932fb8f4bff351a0734bceed2677402c4
KorolkovAlexander <arhontos2@gmail.com>
2024-04-09 12:23:45 +0300

    dsfлr

--- Components/Title.js
+++ Components/Title.js
@@ -3,8 +3,8 @@ import styles from "../styles/title.module.css";
 export default function Title() {
   return (
     <div className={styles.wrap}>
-      <h1>Официальный сайт</h1>
-      <h1>Филиал №14 ГКУ ЛНР РЦСЗН в г.Ровеньки</h1>
+      <h1>Филиал №14</h1>
+      <h1>ГКУ ЛНР РЦСЗН в г.Ровеньки</h1>
       
     </div>
   );
