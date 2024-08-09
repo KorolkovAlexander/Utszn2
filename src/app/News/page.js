569189e106b228489224d890da71d6a614116e9d
KorolkovAlexander <arhontos2@gmail.com>
2024-08-09 14:03:29 +0300

    dsfлrrewedl

--- src/app/News/page.js
+++ src/app/News/page.js
@@ -34,6 +34,7 @@ export default function Page() {
     <Post title={"Прием граждан в пгт Ясеновский"}  src={"/37.jpg"} l={"/News/4"}/>
     <Post title={"Выездной прием граждан в пгт Михайловка"}  src={"/38.jpg"} l={"/News/5"}/>
     <Post title={"Выездной прием граждан в пгт Михайловка"}  src={"/39.jpg"} l={"/News/6"}/>
+    <Post title={"Выездной прием граждан в пгт Михайловка"}  src={"/40.jpg"} l={"/News/7"}/>
       </main>
       <Footer className={styles.footer} />
     </div>
